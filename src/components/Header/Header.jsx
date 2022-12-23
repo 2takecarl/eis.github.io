408a06236d5638fb4e1c00b4614ddf5905fdde8b
1takecarl <55309818+2takecarl@users.noreply.github.com>
2022-12-23 18:49:46 +0100

    Create Header.jsx

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,126 @@
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import CssBaseline from '@mui/material/CssBaseline';
+import Divider from '@mui/material/Divider';
+import Drawer from '@mui/material/Drawer';
+import IconButton from '@mui/material/IconButton';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import MenuIcon from '@mui/icons-material/Menu';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import eis from './EIS.png'
+import { Fab, makeStyles } from '@mui/material';
+
+const drawerWidth = 240;
+const navItems = ['About Us', 'Our Services', 'Contact Sales'];
+
+function DrawerAppBar(props) {
+  const { window } = props;
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen((prevState) => !prevState);
+  };
+
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
+      <Typography variant="h6" sx={{ my: 2 }}>
+        EIS
+      </Typography>
+      <Divider />
+      <List>
+        {navItems.map((item) => (
+          <ListItem key={item} disablePadding>
+            <ListItemButton sx={{ textAlign: 'center' }}>
+              <ListItemText primary={item} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
+  const container = window !== undefined ? () => window().document.body : undefined;
+
+  return (
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      <AppBar component="nav" sx={{background: 'white', color:'black', height: '80px'}}>
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: 'none' } }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography
+            variant="h6"
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+          >
+            <Box sx={{ marginTop: '15px', marginLeft: {sm: '0px', md: '100px', lg: '300px'} }}>
+                <a href='#'>
+                    <img src={eis} alt="" draggable={false} />
+                </a>
+            </Box>
+          </Typography>
+          
+          <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+            {/*navItems.map((item) => (
+              <Button key={item} sx={{ color: '#315EA0', fontWeight: '600', fontSize:"1.4rem", borderRadius: '28' }}>
+                {item}
+              </Button>
+            ))*/}
+            <Fab variant="extended" disableElevation sx={{backgroundColor: '#fff', color: '#314EA0', fontSize: '1rem', textTransform: 'capitalize', fontWeight: '550', boxShadow: 'none', width: '140px', marginRight: '25px'}}>
+                About us
+            </Fab>
+            <Fab variant="extended" disableElevation sx={{backgroundColor: '#fff', color: '#314EA0', fontSize: '1rem', textTransform: 'capitalize', fontWeight: '550', boxShadow: 'none', width: '160px', marginRight: '25px'}}>
+                Our Services
+            </Fab>
+            <Fab variant="extended" disableElevation sx={{backgroundColor: '#fff', color: '#314EA0', fontSize: '1rem', textTransform: 'capitalize', fontWeight: '550', boxShadow: 'none', width: '180px', marginRight: '310px'}}>
+                Contact Sales
+            </Fab>
+          </Box>
+          
+        </Toolbar>
+      </AppBar>
+      <Box component="nav">
+        <Drawer
+          container={container}
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            display: { xs: 'block', sm: 'none' },
+            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+          }}
+        >
+          {drawer}
+        </Drawer>
+      </Box>
+      
+    </Box>
+  );
+}
+
+DrawerAppBar.propTypes = {
+  /**
+   * Injected by the documentation to work in an iframe.
+   * You won't need it on your project.
+   */
+  window: PropTypes.func,
+};
+
+export default DrawerAppBar;
