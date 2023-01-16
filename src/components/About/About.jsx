2bbce772bdc19dda9379c9abc72b6aea58595b20
2takecarl <carlsterner@outlook.com>
2023-01-16 19:46:17 +0100

    4rd commit

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -5,11 +5,11 @@ import react from 'react'
 const About = () => {
 
     return (
-        <Box sx={{display: 'flex', flexDirection: 'row'}}>
-            <Box sx={{marginLeft: '150px', marginTop: '200px', fontSize: '1.45rem', maxWidth: '500px'}}>
-                <Box sx={{fontSize: '2rem'}}> <b>ECS is a multilingual company supplying Simultaneous Interpretation </b></Box> services to companies and organizations. Specialized in European Works Councils, it supplies interpretation and technical solutions to companies all over Europe. It's widespread network of interpreters as well as technical competences has a large history of very successful events. 
+        <Box sx={{display: 'flex', flexDirection: {lg: 'row', xs: 'column'} }}>
+            <Box sx={{marginLeft: {lg: '150px', xs: '25%'}, marginRight: '40px', marginTop: '200px', fontSize: '1.45rem', maxWidth: '500px'}}>
+                <Box sx={{fontSize: {lg: '1.75rem', xs: "1.25rem"}}}> <Box sx={{fontSize: {lg: '2rem', xs: '1.5rem'}}}><b>ECS is a multilingual company supplying Simultaneous Interpretation </b></Box> services to companies and organizations. Specialized in European Works Councils, it supplies interpretation and technical solutions to companies all over Europe. It's widespread network of interpreters as well as technical competences has a large history of very successful events. </Box>
             </Box>
-            <Box sx={{marginTop: '200px', marginLeft: '50px'}}>
+            <Box sx={{marginTop: {lg: '200px', xs:'50px'}, marginLeft: {lg: '50px', xs: 'calc(25% - 50px)'}}}>
                 <img src={img} alt=""/>
             </Box>
         </Box>
--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,15 +1,21 @@
 import { Box } from '@mui/system'
 import react from 'react'
+import img from './img.png'
 
 const Services = () => {
 
     return (
-        <Box sx={{marginTop: '150px'}}>
-            <Box>
-                One stop for all your language needs
+        <Box sx={{marginTop: '150px', display: 'flex', flexDirection:{xs: 'column', lg:'row'}}}>
+            <Box sx={{textAlign:{xs: 'center', lg:'left'}, mx:"40px"}}>
+                <Box sx={{fontSize: '2rem', fontWeight: '700'}}>
+                    One stop for all your language needs
+                </Box>
+                <Box sx={{fontSize: '1.5rem'}}>
+                    our depth of language expertise and experience allows us to break down language barriers and solve any form of communicatiion challenge. Any language, any project, any time. We offer interpretation from and in to all European languages.
+                </Box>
             </Box>
-            <Box>
-                our depth of language expertise and experience allows us to break down language barriers and solve any form of communicatiion challenge. Any language, any project, any time. We offer interpretation from and in to all European languages.
+            <Box sx={{mx: {xs: 'auto', lg:'80px'}, mt:{xs: '40px', lg:"0"}}}>
+                <img src={img} alt="" />
             </Box>
         </Box>
     )
