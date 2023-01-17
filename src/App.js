d1745563329a419dc3a3393e10e99047b536405c
2takecarl <carlsterner@outlook.com>
2023-01-17 19:28:35 +0100

    -a A.js #

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { Routes, Route, HashRouter, Link} from 'react-router-dom';
 
 function App() {
   return (
-    <HashRouter basename="/">
+    <HashRouter basename="/#">
         <Header />
         <Routes>
           <Route path="/" element={<Hero />} />
