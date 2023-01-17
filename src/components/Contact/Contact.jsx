aa95de93bbcde253ccc3e44becb855b8cc7e6395
2takecarl <carlsterner@outlook.com>
2023-01-17 21:48:52 +0100

    grdrgdrg

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React from 'react';
 import { useRef } from 'react';
 import { Grid, TextField, Button, Card, CardContent, Typography, Modal } from '@mui/material';
 import { Box } from '@mui/system';
