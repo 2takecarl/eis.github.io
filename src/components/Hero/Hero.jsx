98c6bf4647b4e5ac646ca9fc597d7a10e052d0e5
2takecarl <carlsterner@outlook.com>
2023-01-17 20:24:08 +0100

    get a quote button fix

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -36,8 +36,10 @@ const Hero = () => {
         </Box>
 
         <Box sx={{display: 'flex', justifyContent: 'center'}}>
-            <StyledButton href="/contact" draggable={false} size='large' sx={{borderRadius: 28, backgroundColor: '#EDF1F6', paddingLeft: '20px', paddingRight: '20px', fontFamily: `"Source Sans Pro", sans-serif`, marginTop: '20px', fontWeight: '600', fontSize: '1.35rem', color: '#315EA0'}}>
-                Get a Quote    
+            <StyledButton draggable={false} size='large' sx={{borderRadius: 28, backgroundColor: '#EDF1F6', paddingLeft: '20px', paddingRight: '20px', fontFamily: `"Source Sans Pro", sans-serif`, marginTop: '20px', fontWeight: '600', fontSize: '1.35rem', color: '#315EA0'}}>
+                <Link to="/contact">
+                    Get a Quote    
+                </Link>
             </StyledButton>        
         </Box>
 
