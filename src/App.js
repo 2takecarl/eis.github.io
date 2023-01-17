25ef744fe17ebc7975bddcc7a21ebeb363f8f846
2takecarl <carlsterner@outlook.com>
2023-01-17 19:04:48 +0100

    asefasef

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
     <HashRouter basename="/">
         <Header />
         <Routes>
-          <Route path="/" element={<Hero />} />
+          <Route basename="/eis.github.io" path="/" element={<Hero />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/services" element={<Services />} />
